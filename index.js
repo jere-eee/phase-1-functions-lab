8b5b13b9ffc6784e824debb5ab8350597c5deebe
Akanle Jeremy <akanlejeremy@gmail.com>
2024-09-23 14:47:22 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(val){
+    return Math.abs(42 - val);
+}
+
+function distanceFromHqInFeet(val) {
+    return distanceFromHqInBlocks(val) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
+        return 0;   
+    } else if (distance <= 2000) {
+        return ((distance-400)*2)/100;
+    } else if (distance <= 2500) {
+        return 25;
+    } else return "cannot travel that far";
+}
